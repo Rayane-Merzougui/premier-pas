beed704673b888069f3f55ab4df4603e9539a45a
rayane <rayane.merzougui.work@gmail.com>
2024-07-14 11:56:29 +0100

    on ajoute du js

---
+++ index.js
@@ -0,0 +1,5 @@
+const mousemove = document.querySelector(".mousemove");
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
