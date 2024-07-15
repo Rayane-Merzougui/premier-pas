f793ad32a5ee5f5efd252664ae3b1842a421a532
rayane <rayane.merzougui.work@gmail.com>
2024-07-15 18:11:40 +0100

    on avance avec une nav bar

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const mousemove = document.querySelector(".mousemove");
+const navbar = document.querySelector("nav");
 window.addEventListener("mousemove", (e) => {
   mousemove.style.left = e.pageX + "px";
   mousemove.style.top = e.pageY + "px";
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    navbar.style.top = 0;
+  } else if (window.scrollY < 120) {
+    navbar.style.top = -50 + "px";
+  }
 });
